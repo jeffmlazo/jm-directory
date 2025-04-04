2deecf842df0bdc17f0c42e861718cc7ae52c09b
Jeff Ray <jeffmlazo@yahoo.com>
2025-04-05 02:25:11 +0800

    Add global css styles

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: ['@tailwindcss/postcss'],
+  // 'tailwindcss/nesting',
+  // ['tailwindcss', { config: './custom-tailwind-config.js' }],
+  // ['autoprefixer', { grid: true }],
+  // ['postcss-custom-properties', { preserve: false }],
+  // 'postcss-flexbugs-fixes',
 };
 
 export default config;
