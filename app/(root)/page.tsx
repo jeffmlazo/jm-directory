4871671c3314226651fe48ff1abc4bbce554ab65
Jeff Ray <jeffmlazo@yahoo.com>
2025-04-16 00:25:46 +0800

    Add sanity client package Add experimental & devIndicators for next config Add sanityFetch & SanityLive imports Add views indicators at the bottom right corner Add write client with token Add STARTUP_BY_ID_QUERY and STARTUP_VIEWS_QUERY Update logo

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,15 @@
 
 import SearchForm from "@/components/SearchForm";
 import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+// import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string}>}) {
 
   const query = (await searchParams).query;
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params});
 
   return (
    <>
@@ -34,6 +36,8 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         )}
       </ul>
    </section>
+
+  <SanityLive />
    </>
   );
 }
