03a240dfdf63899d7382521ec0c464c91d79120f
Jeff Ray <jeffmlazo@yahoo.com>
2025-04-19 02:25:54 +0800

    Add server actions for startup create form submit Add parseServerActionResponse for JSON Add global toaster in the layout Add toaster notification for startup form Add router redirection for single startup page after successfully form submit

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -8,10 +8,15 @@ import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
 import { formSchema } from "@/lib/validation";
 import { z } from "zod";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
 
 const StartupForm = () => {
     const [errors, setErrors] = useState<Record<string, string>>({});
     const [pitch, setPitch] = useState("");
+    const { toast } = useToast();
+    const router = useRouter();
     const handleFormSubmit = async (prevState: any, formData: FormData) => {
         try {
             const formValues = {
@@ -24,18 +29,38 @@ const StartupForm = () => {
     
             await formSchema.parseAsync(formValues);
     
-            console.log(formValues);
-            // const result = await createIdea(prevState, formData, pitch);
-            // console.log(result);
+            const result = await createPitch(prevState, formData, pitch);
 
+            if(result.status === "SUCCESS") {
+                toast({
+                    title: "Success",
+                    description: "Your startup pitch has been created successfully",
+                });
+
+                router.push(`/startup/${result._id}`);
+            }
+
+            return result;
         } catch (error) {
             if(error instanceof z.ZodError) {
                 const fieldErrors = error.flatten().fieldErrors;
 
                 setErrors(fieldErrors as unknown as Record<string, string>);
 
+                toast({
+                    title: "Error",
+                    description: "Please check your inputs and try again",
+                    variant: "destructive",
+                });
+
                 return { ...prevState, error: 'Validation failed', status: 'ERROR'}
             }
+
+            toast({
+                title: "Error",
+                description: "Please check your inputs and try again",
+                variant: "destructive",
+            });
 
             return {
                 ...prevState,
