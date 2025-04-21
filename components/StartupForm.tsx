76776a878c96fbb20b22f6e61f38f8681b01fc00
Jeff Ray <jeffmlazo@yahoo.com>
2025-04-22 02:01:53 +0800

    Add parallel request for post & editors picks queries Add playlist query by slug Add listing of editors picks below of a single startup page Add background color of toast notification for creating startup page

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -17,6 +17,7 @@ const StartupForm = () => {
     const [pitch, setPitch] = useState("");
     const { toast } = useToast();
     const router = useRouter();
+
     const handleFormSubmit = async (prevState: any, formData: FormData) => {
         try {
             const formValues = {
